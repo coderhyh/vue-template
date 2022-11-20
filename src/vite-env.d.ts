18c1a7e3f6c3231fb6c0676bb6c7fa7d9e8fc202
coderhyh <772567615@qq.com>
2022-11-20 19:42:16 +0800

    feat(环境变量): 新增环境变量类型

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,3 +5,11 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+
+interface ImportMetaEnv {
+  readonly VITE_SOCKET_BASE_URL: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
