68d2fcdbd6627e287624cbb05c67ce564eb78454
coderhyh <772567615@qq.com>
2022-11-25 15:16:35 +0800

    feat(deps                           (项目依赖)): 添加打包依赖terser

--- src/components.d.ts
+++ src/components.d.ts
@@ -7,12 +7,8 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    Commons: typeof import('./components/commons/commons.vue')['default']
     Cpn: typeof import('./components/cpn.vue')['default']
-    ElButton: typeof import('element-plus/es')['ElButton']
     ElConfigProvider: typeof import('element-plus/es')['ElConfigProvider']
-    ElTree: typeof import('element-plus/es')['ElTree']
-    HelloWorld: typeof import('./components/HelloWorld.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
