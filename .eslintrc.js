0f886cc23da7daca8f57147889763d36823c31fa
coderhyh <772567615@qq.com>
2023-12-07 11:28:01 +0800

    chore: update

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   root: true,
   env: {
     browser: true,
+    es2023: true,
     node: true
   },
   extends: [
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,4 @@
-{
+module.exports = {
   "useTabs": false,
   "tabWidth": 2,
   "printWidth": 120,
