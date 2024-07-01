0af90f8fc67b8556a8c1f1e4400d99207b6d83c4
coderhyh <772567615@qq.com>
2024-07-01 15:18:48 +0800

    feat: update vue-router

--- src/views/index/src/index.vue
+++ src/views/index/src/index.vue
@@ -1,14 +1,19 @@
 <template>
-  <div class="index">
-    <h1>Index</h1>
+  <div class=":uno: index :uno:">
+    <h1 w-2xl text-cyan>
+      Index
+    </h1>
     <Cpn />
     <Cpn1 />
     <div class="i-ph-anchor-simple-thin" />
     <div class="i-mdi-alarm text-orange-400" />
     <div class="i-logos-vue text-3xl" />
     <div class="i-twemoji-grinning-face-with-smiling-eyes hover:i-twemoji-face-with-tears-of-joy" />
-    <div class=":uno-button: m-2 h-100 w-210 b-(1 red solid) bg-white text-red font-bold">
+    <div class=":uno-bbb: m-2 h-10 w-20 b-(1 red solid) bg-white text-red font-bold">
       11
+    </div>
+    <div class=":uno-aaa: text-pink">
+      666
     </div>
   </div>
 </template>
